cdf02057635cc24cec17f1a8d5908bd22e78b3af
rtokromo <riandie@gmail.com>
2025-07-18 19:27:15 -0300

    Update constants.js

---
+++ src/js/config/constants.js
@@ -0,0 +1,12 @@
+export const JAVANESE_MONTHS = [
+    ["Koso Soworo (1)", 36],
+    ["Pobo (2)", 35],
+    // ... rest van de maanden
+];
+
+export const REFERENCE_DATES = {
+    MODERN: new Date(2023, 7, 5),
+    CLASSIC: new Date(1975, 11, 20)
+};
+
+export const JAVANESE_YEAR_LENGTH = 355;

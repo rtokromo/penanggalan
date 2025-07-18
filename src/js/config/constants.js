d39fb17d934dbb44f2d62ad444cfce03c0ae8e6b
rtokromo <riandie@gmail.com>
2025-07-18 19:31:31 -0300

    Update constants.js

--- src/js/config/constants.js
+++ src/js/config/constants.js
@@ -1,12 +1,45 @@
 export const JAVANESE_MONTHS = [
     ["Koso Soworo (1)", 36],
     ["Pobo (2)", 35],
-    // ... rest van de maanden
+    ["Mono (3)", 36],
+    ["Somo (4)", 35],
+    ["Poko (5)", 36],
+    ["Bowoko (6)", 35],
+    ["Pono (7)", 36],
+    ["Woko (8)", 35],
+    ["Sono (9)", 36],
+    ["Noto (10)", 35]
 ];
 
+export const PASARAN_DATA = [
+    { naam: "Kliwon", waarde: 8, verkort: "Kl" },
+    { naam: "Legi", waarde: 5, verkort: "Lg" },
+    { naam: "Pahing", waarde: 9, verkort: "Pa" },
+    { naam: "Pon", waarde: 7, verkort: "Po" },
+    { naam: "Wage", waarde: 4, verkort: "Wg" }
+];
+
+export const SPECIALE_JAVAANSE_DAGEN = {
+    1: [1, 11, 21, 31],
+    2: [2, 12, 22, 32],
+    3: [3, 13, 23, 33],
+    4: [4, 14, 24, 34],
+    5: [5, 15, 25, 35],
+    6: [6, 16, 26],
+    7: [7, 17, 27],
+    8: [8, 18, 28],
+    9: [9, 19, 29],
+    10: [10, 20, 30]
+};
+
 export const REFERENCE_DATES = {
-    MODERN: new Date(2023, 7, 5),
-    CLASSIC: new Date(1975, 11, 20)
+    MODERN: new Date(2023, 7, 5),  // 5 augustus 2023
+    CLASSIC: new Date(1975, 11, 20) // 20 december 1975
+};
+
+export const REFERENCE_JAVAANSE_YEARS = {
+    MODERN: 6449,
+    CLASSIC: 6400
 };
 
 export const JAVANESE_YEAR_LENGTH = 355;

8f0912f1177c1a55318926726b8f1453bff7bbe8
rtokromo <riandie@gmail.com>
2025-07-18 19:26:04 -0300

    Update dateConverter.js

---
+++ src/js/modules/dateConverter.js
@@ -0,0 +1,12 @@
+import { 
+    REFERENCE_DATES,
+    JAVANESE_YEAR_LENGTH 
+} from '../config/constants';
+
+export function convertToJavaneseDate(gregorianDate) {
+    // Implementatie conversie logica
+}
+
+export function convertToGregorianDate(javaneseYear, javaneseMonth, javaneseDay) {
+    // Implementatie conversie logica
+}
